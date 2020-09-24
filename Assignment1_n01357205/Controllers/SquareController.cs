baf7c7e9f7e7306724a65f3fdefd8e7e4f35cc2b
jerrineldo <jerin.jollybhavan.gmail.com>
2020-09-25 00:31:03 +0530

    Adding Square Controller

---
+++ Assignment1_n01357205/Controllers/SquareController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment1_n01357205.Controllers
+{
+    public class SquareController : ApiController
+    {
+        //GET api/Square/5
+        //input : integer
+        //output: Returns the square of the integer input
+        public int Get(int id)
+        {
+            return id * id;
+        }
+    }
+}
